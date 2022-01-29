d62ae099ef99d2ffc4e87ba910a17907226e7ef3
enderpicture <enderpicture@gmail.com>
2022-01-29 15:56:28 -0800

    nav buttons added

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -32,7 +32,25 @@
 			padding: 0.5rem $side-padding;
 		}
 		a {
+			position: relative;
+			z-index: 1;
+			&::before {
+				content: '';
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+				background-color: #fff;
+				opacity: 0;
+				z-index: -1;
+				transition: opacity .5s ease;
+			}
 			text-decoration: none;
+			&:hover::before {
+				opacity: 0.1;
+				transition: opacity .2s ease;
+			}
 		}
 		.home {
 			font-weight: 900;
